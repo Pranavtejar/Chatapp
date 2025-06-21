a3d409f7fc3461120ca83369fea10981964a6334
pranavteja <145261027+Pranavtejar@users.noreply.github.com>
2025-06-21 08:23:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, redirect, url_for, request, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -9,11 +12,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SECRET_KEY"] = "helloguys"
 
 db = SQLAlchemy(app)
-
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,17 +23,8 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(100), nullable=False)
     friends = db.relationship('Friend', backref='user', lazy=True)
 
-    def is_active(self):
-        return True  
-
-    def is_authenticated(self):
-        return True  
-
-    def is_anonymous(self):
-        return False  
-
     def get_id(self):
-        return str(self.id)  
+        return str(self.id)
 
 class Friend(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,7 +36,7 @@ with app.app_context():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 @app.route('/')
 def home():
@@ -57,12 +49,11 @@ def sign_up():
         username = request.form["username"]
         password = request.form["password"]
         name = request.form["name"]
-        
+
         if not username or not password or not name:
             error_message = "Please fill in all fields."
         else:
             existing_user = User.query.filter_by(username=username).first()
-
             if existing_user:
                 error_message = "User exists, please choose a different username."
             else:
@@ -71,8 +62,9 @@ def sign_up():
                 db.session.add(new_user)
                 db.session.commit()
                 login_user(new_user)
-                session['username'] = username  
+                session['username'] = username
                 return redirect(url_for("chat", friend=username))
+
     return render_template("signup.html", error_message=error_message)
 
 @app.route('/login', methods=["GET", "POST"])
@@ -81,22 +73,21 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        
+
         user = User.query.filter_by(username=username).first()
-        
         if user and check_password_hash(user.password, password):
             login_user(user)
-            session['username'] = user.username  
+            session['username'] = user.username
             return redirect(url_for("chat", friend=username))
         else:
             error_message = "Invalid username or password."
-    
+
     return render_template("login.html", error_message=error_message)
 
 @app.route('/logout')
 def logout():
     logout_user()
-    session.pop('username', None)  
+    session.pop('username', None)
     return redirect(url_for("home"))
 
 @app.route('/add_friend', methods=["GET", "POST"])
@@ -105,12 +96,12 @@ def add_friend():
     error_message = None
     if request.method == "POST":
         friend_username = request.form["friend_username"]
-        user = User.query.filter_by(username=current_user.username).first()
+        user = current_user
         friend = User.query.filter_by(username=friend_username).first()
 
         if not friend:
             error_message = "Friend not found"
-        elif friend_username == current_user.username:
+        elif friend_username == user.username:
             error_message = "You cannot add yourself as a friend."
         else:
             existing_friend = Friend.query.filter_by(user_id=user.id, friend_username=friend_username).first()
@@ -127,7 +118,7 @@ def add_friend():
 @app.route('/friends')
 @login_required
 def get_friends():
-    user = User.query.filter_by(username=current_user.username).first()
+    user = current_user
     friends = [friend.friend_username for friend in user.friends]
     return jsonify(friends)
 
@@ -142,49 +133,27 @@ users_db = {}
 def handle_join(data):
     username = data['username']
     users_db[username] = request.sid
-    emit('user_list', list(users_db.keys()), broadcast=True)
+    emit('user_joined', {'user': username}, broadcast=True)
 
 @socketio.on('message')
 def handle_message(data):
-    sender_username = None
-    for user, sid in users_db.items():
-        if sid == request.sid:
-            sender_username = user
-            break
-
-    target_user_sid = users_db.get(data['target_user'])
-    if target_user_sid and sender_username != data['target_user']:
-        emit('message', {
-            'sender': sender_username,
-            'message': data['message'],
-            'target_user': data['target_user']
-        }, room=target_user_sid)
+    recipient = data['to']
+    sender = data['from']
+    message = data['message']
+    sid = users_db.get(recipient)
+    if sid:
+        emit('message', {'from': sender, 'message': message}, room=sid)
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    for user, sid in list(users_db.items()):
+    username = None
+    for user, sid in users_db.items():
         if sid == request.sid:
-            del users_db[user]
-            emit('user_list', list(users_db.keys()), broadcast=True)
+            username = user
             break
-
-@socketio.on('webrtc_offer')
-def handle_webrtc_offer(data):
-    target_user_sid = users_db.get(data['target_user'])
-    if target_user_sid:
-        emit('webrtc_offer', data, room=target_user_sid)
-
-@socketio.on('webrtc_answer')
-def handle_webrtc_answer(data):
-    target_user_sid = users_db.get(data['target_user'])
-    if target_user_sid:
-        emit('webrtc_answer', data, room=target_user_sid)
-
-@socketio.on('webrtc_ice_candidate')
-def handle_webrtc_ice_candidate(data):
-    target_user_sid = users_db.get(data['target_user'])
-    if target_user_sid:
-        emit('webrtc_ice_candidate', data, room=target_user_sid)
+    if username:
+        del users_db[username]
+        emit('user_left', {'user': username}, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000)
